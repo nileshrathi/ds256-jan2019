01405db2479998f98452fa9693c261546167400b
Swapnil-Gandhi <gandhis@iisc.ac.in>
2019-03-19 02:51:15 +0530

    Checked-in Nilesh's Code Submission

--- ds256-a1/Code/src/in/ds256/Assignment1/giraph/sssp.java
+++ ds256-a1/Code/src/in/ds256/Assignment1/giraph/sssp.java
@@ -1,7 +1,6 @@
 package in.ds256.Assignment1.giraph;
 
 import org.apache.giraph.graph.BasicComputation;
-import org.apache.giraph.conf.GiraphConfiguration;
 import org.apache.giraph.conf.LongConfOption;
 import org.apache.giraph.edge.Edge;
 import org.apache.giraph.graph.Vertex;
@@ -20,7 +19,7 @@ public class sssp extends BasicComputation<LongWritable, DoubleWritable, FloatWr
 
 	/** Class logger */
 	private static final Logger LOG = Logger.getLogger(sssp.class);
-	
+
 	/**
 	 * Is this vertex the source id?
 	 *
--- ds256-a1/Code/src/in/ds256/Assignment1/ktruss.java
+++ ds256-a1/Code/src/in/ds256/Assignment1/ktruss.java
@@ -1,9 +1,6 @@
 package in.ds256.Assignment1;
 
 import java.io.IOException;
-import java.util.Scanner;
-import java.util.regex.Pattern;
-
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
@@ -12,7 +9,7 @@ public class ktruss {
 
 	public static void main(String[] args) throws IOException {
 		
-		/*String inputFile = args[0]; // Should be some file on HDFS
+		String inputFile = args[0]; // Should be some file on HDFS
 		String outputFile = args[1]; // Should be some file on HDFS
 		
 		SparkConf sparkConf = new SparkConf().setAppName("KTruss");
@@ -20,23 +17,12 @@ public class ktruss {
 
 		JavaRDD<String> inputRDD = sc.textFile(inputFile);
 		
-		*//**
+		/**
 		 * Code goes here...
-		 *//*
+		 */
 		
 		sc.stop();
-		sc.close();*/
-		while(true)
-		{
-		 Pattern SEPARATOR = Pattern.compile("[\t | \\s* | ,]");
-		Scanner sc = new Scanner(System.in);
-		String tokens[] = SEPARATOR.split(sc.nextLine());
-		for (String a : tokens) {
-			System.out.print(a+" ");
-			
-		}
-		System.out.println();
-		}
+		sc.close();
 		
 	}
 }
