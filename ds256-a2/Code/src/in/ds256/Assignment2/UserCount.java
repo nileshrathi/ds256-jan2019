93fb75097df571adf2e7d1a9f3fd5826c41711d4
nileshrathi <46645439+nileshrathi@users.noreply.github.com>
2019-04-16 18:41:42 +0530

    Update UserCount.java

--- ds256-a2/Code/src/in/ds256/Assignment2/UserCount.java
+++ ds256-a2/Code/src/in/ds256/Assignment2/UserCount.java
@@ -1,9 +1,16 @@
 package in.ds256.Assignment2;
 
 import java.util.HashMap;
+import java.util.Locale;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.Collections;
+import java.util.Date;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import scala.Tuple2;
@@ -14,7 +21,14 @@ import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.spark.streaming.kafka010.ConsumerStrategies;
 import org.apache.spark.streaming.kafka010.KafkaUtils;
 import org.apache.spark.streaming.kafka010.LocationStrategies;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.PairFunction;
 import org.apache.spark.streaming.api.java.*;
 import org.apache.spark.streaming.Durations;
 
@@ -43,16 +57,92 @@ public class UserCount {
 		kafkaParams.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
 		kafkaParams.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
 
-		// Create direct kafka stream with broker and topic
+		
 		JavaInputDStream<ConsumerRecord<String, String>> messages = KafkaUtils.createDirectStream(jssc,
 				LocationStrategies.PreferConsistent(), ConsumerStrategies.Subscribe(Collections.singleton(topic), kafkaParams));
 
 		/**
 		*	Code goes here....
 		*/
+		
+JavaDStream<String> input_lines = messages.map(ConsumerRecord::value).map(json -> {
+			
+			try {
+				JSONParser jsonParser = new JSONParser();
+				String epoch_time,timezone,lang,tweet_id,user_id,followers_count,friends_count,normalised_tweet,hashtags;
+				JSONObject js = null;
+				
+					js = (JSONObject) jsonParser.parse(json);
+				
+				
+				JSONObject user;
+				user = (JSONObject)js.get("user");
+				epoch_time=(String)js.get("created_at");
+				
+					lang=(String)js.get("lang");
+			 
+				
+				
+					user_id = (String)user.get("id_str");
+									timezone=(String)user.get("time_zone");
+					String created_at=epoch_time;
+		
+									DateFormat outputFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm");
+								    DateFormat inputFormat = new SimpleDateFormat("EEE MMM dd HH:mm:ss ZZZZZ yyyy", Locale.ENGLISH);
+								    inputFormat.setLenient(true);
 
+								    Date date = inputFormat.parse(created_at);
+								    
+								    Calendar cal = Calendar.getInstance();
+								    cal.setTime(date);
+								    Integer hours = cal.get(Calendar.HOUR_OF_DAY);
+								    
+								    Integer year= cal.get(Calendar.YEAR);
+								    Integer month= cal.get(Calendar.MONTH);
+								    Integer day= cal.get(Calendar.DATE);
+								    
+								    String time= year.toString() + "-" + month.toString() + "-" + day.toString() + "-" + hours.toString();
+				
+				
+
+				return time+"----"+user_id;
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				return "cache block";
+			}
+			
+		}).transform(x->x.distinct());
+		/*
+		csv_row.foreachRDD( row-> {
+	        row.collect().stream().forEach(n-> System.out.println(n));
+	    });
+	    */
+JavaPairDStream<String, String> timeToUser=input_lines.mapToPair(new PairFunction<String, String, String>() {
+    public Tuple2<String, String> call(String arg0) throws Exception {
+    	String[] split=arg0.split("----");
+    	String time=split[0];
+    	String userid=split[1];
+        return new Tuple2<String,String> (time,userid);
+    }
+});
+JavaPairDStream<String, Iterable<String>> timetoUniqueUserGroup = timeToUser.groupByKey();
+
+
+     
+timetoUniqueUserGroup.print();
+		
+
+		/*
+		csv_row.foreachRDD(str -> {
+			str.saveAsTextFile(output);
+		});
+		*/
+timetoUniqueUserGroup.dstream().saveAsTextFiles(output,"");
+		
 		// Start the computation
 		jssc.start();
 		jssc.awaitTermination();
 	}
 }
+
+
